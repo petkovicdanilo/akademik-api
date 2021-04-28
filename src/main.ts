7d58aa4190462caabda47936d802edeb35bbf414
Danilo <petkovicdanilo97@gmail.com>
2021-04-28 14:45:51 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+import * as helmet from "helmet";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("UniversityPortal API")
+    .setDescription(
+      "API for application used by university students and professors",
+    )
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  app.use(helmet());
+
+  await app.listen(3000);
+}
+bootstrap();

739bdf6eaed62f3d375932688c55713608708843
Danilo <petkovicdanilo97@gmail.com>
2021-05-27 16:01:48 +0200

    Add swagger info for UserType

--- src/users/dto/user-with-type.dto.ts
+++ src/users/dto/user-with-type.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { UserType } from "../types";
 import { UserDto } from "./user.dto";
 
 export class UserWithTypeDto extends UserDto {
+  @ApiProperty({
+    enum: ["student", "professor"],
+    type: "string",
+  })
   type: UserType;
 }

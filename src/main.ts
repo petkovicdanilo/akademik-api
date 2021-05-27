a1d67dc5016d90967654ac280b35c434258e12d9
Danilo <petkovicdanilo97@gmail.com>
2021-05-27 20:59:55 +0200

    Add validation libraries

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import * as helmet from "helmet";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +21,8 @@ async function bootstrap() {
   app.use(helmet());
 
   app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT);
 }

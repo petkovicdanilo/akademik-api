75d503a684351cb69225233bf870b5f7931bc91c
Danilo <petkovicdanilo97@gmail.com>
2021-05-30 23:57:32 +0200

    Add admin guard

---
+++ src/auth/strategies/admin.strategy.ts
@@ -0,0 +1,26 @@
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { PassportStrategy } from "@nestjs/passport";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { ProfilesService } from "src/users/profiles/profiles.service";
+
+@Injectable()
+export class AdminStrategy extends PassportStrategy(Strategy, "admin") {
+  constructor(private readonly profileService: ProfilesService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: `${process.env.JWT_SECRET}`,
+    });
+  }
+
+  async validate(payload: any) {
+    const id = payload.user.id;
+    const type = payload.user.type;
+
+    if (type != "admin") {
+      throw new UnauthorizedException("User is not admin");
+    }
+
+    return { id, type };
+  }
+}
